bc7d0aaec2857d2c020cf02842772fdc1c1c7e25
pedrofernandescajuri <116416776+pedrofernandescajuri@users.noreply.github.com>
2022-12-12 19:56:59 -0300

    up

---
+++ Desafios/Desafio005/script.js
@@ -0,0 +1,9 @@
+function verifique() {
+    resu = window.document.getElementById('res')
+    nasc = window.document.getElementById('inas')
+    sexm = window.document.getElementById('imas')
+    sexf = window.document.getElementById('ifem')
+    if (nasc - 2022 <= 10) {
+        Img.src = 'Imagens.Menino.jpg'
+    }
+}
