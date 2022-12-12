98582e5d952f8ac594bdb7368d007eb9c66f9a35
pedrofernandescajuri <116416776+pedrofernandescajuri@users.noreply.github.com>
2022-12-12 10:07:19 -0300

    update

---
+++ Aulas/Aula05/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+if (vel > 60) {
+    console.log(`A velocidade do seu carro e igual a ${vel}Km/h`)
+    console.log('Multado!!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ Aulas/Aula05/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Nascendo em ${país}`)
+if (país != 'Brasil') {
+    console.log('Você e estrangeiro')
+}
+else {
+    console.log('Você e Brasileiro')
+}
