8f41e021b5ceb35b703cf8ccde0c863abece2fc4
pedrofernandescajuri <116416776+pedrofernandescajuri@users.noreply.github.com>
2022-12-14 20:05:30 -0300

    update

--- Desafios/Desafio009/script.js
+++ Desafios/Desafio009/script.js
@@ -1,14 +1,31 @@
-function clicou() {
-    var n1 = document.getElementById('in1')
-    var n2 = document.getElementById('in2')
-    var n3 = document.getElementById('in3')
-    var res = document.getElementById('ires')
-    var nn1 = Number(n1.value)
-    var nn2 = Number(n2.value)
-    var nn3 = Number(n3.value)
-    var soma = nn1 + nn3
-    while(soma < nn2){
-        soma
-        res.innerHTML = (`> ${soma}`)
+function contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        window.alert('[Erro] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1.')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i;c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else{
+            // Contagem decrescente
+            for(let c = i;c >= f;c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
